f22d9547925aaa609acdec577d1c77b6a8e87a50
Fernando Rodrigues da Roza <froza@padtec.com.br>
2018-12-05 21:23:41 -0200

    - Added content page get and save for export
    Signed-off-by: Fernando Rodrigues da Roza <froza@padtec.com.br>

--- js/rdfa-marker.js
+++ js/rdfa-marker.js
@@ -1,44 +1,6 @@
-var menuExport = {
-  "id" : "Export",
-  "title" : "Export RDFa Marker"
-};
-
-var menuItem = {
-  "id" : "root",
-  "title" : "Adicionar marcação RDFa a seleção",
-  "contexts" : [ "selection" ]
-};
-
-var menuSubject = {
-  "id" : "itemSubject",
-  "parentId" : "root",
-  "title" : "Marcar seleção como Sujeito",
-  "contexts" : [ "selection" ]
-};
-
-var menuPredicate = {
-  "id" : "itemPredicate",
-  "parentId" : "root",
-  "title" : "Marcar seleção como Predicado",
-  "contexts" : [ "selection" ]
-};
-
-var menuObject = {
-  "id" : "itemObject",
-  "parentId" : "root",
-  "title" : "Marcar seleção como Objeto",
-  "contexts" : [ "selection" ]
-};
-
 function isInt(value) {
   return !isNaN(value) && parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
 }
-
-chrome.contextMenus.create(menuExport);
-chrome.contextMenus.create(menuItem);
-chrome.contextMenus.create(menuSubject);
-chrome.contextMenus.create(menuPredicate);
-chrome.contextMenus.create(menuObject);
 
 function createSubMenus() {
   createSubMenuSubject("http://dbpedia.org/data3/Person.json", "itemSubject");
@@ -69,52 +31,86 @@ function createSubMenu(id, parentId, title) {
   };
 }
 
-createSubMenus();
+$(document).ready(function() {
+  chrome.contextMenus.removeAll(function() {
+  });
 
-chrome.contextMenus.onClicked.addListener(function(clickData) {
-  if (clickData.menuItemId == "Export") {
-    pageHTML = document.documentElement.outerHTML;
-    console.log(pageHTML);
-    chrome.storage.local.remove([ 'pageHTML' ], function() {
-    });
-    chrome.storage.local.set({
-      'pageHTML' : pageHTML
-    }, function() {
-    });
-    chrome.tabs.create({
-      url : '/html/export.html'
-    });
-  }
-  if (clickData.menuItemId == "root" && clickData.selectionText) {
-    if (isInt(clickData.selectionText)) {
-      chrome.storage.sync.get([ 'total', 'limit' ], function(budget) {
-        var newTotal = 0;
-        if (budget.total) {
-          newTotal += parseInt(budget.total);
-        }
+  var menuExport = {
+    "id" : "Export",
+    "title" : "Export RDFa Marker"
+  };
 
-        newTotal += parseInt(clickData.selectionText);
-        chrome.storage.sync.set({
-          'total' : newTotal
-        }, function() {
-          if (newTotal >= budget.limit) {
-            var notifOptions = {
-              type : "basic",
-              iconUrl : "icon48.png",
-              title : "Limit reached!",
-              message : "Uh oh, look's like you've reached your alloted limit."
-            };
-            chrome.notifications.create('limitNotif', notifOptions);
+  var menuItem = {
+    "id" : "root",
+    "title" : "Adicionar marcação RDFa a seleção",
+    "contexts" : [ "selection" ]
+  };
 
-          }
-        });
+  var menuSubject = {
+    "id" : "itemSubject",
+    "parentId" : "root",
+    "title" : "Marcar seleção como Sujeito",
+    "contexts" : [ "selection" ]
+  };
+
+  var menuPredicate = {
+    "id" : "itemPredicate",
+    "parentId" : "root",
+    "title" : "Marcar seleção como Predicado",
+    "contexts" : [ "selection" ]
+  };
+
+  var menuObject = {
+    "id" : "itemObject",
+    "parentId" : "root",
+    "title" : "Marcar seleção como Objeto",
+    "contexts" : [ "selection" ]
+  };
+
+  chrome.contextMenus.create(menuExport);
+  chrome.contextMenus.create(menuItem);
+  chrome.contextMenus.create(menuSubject);
+  chrome.contextMenus.create(menuPredicate);
+  chrome.contextMenus.create(menuObject);
+
+  createSubMenus();
+
+  chrome.contextMenus.onClicked.addListener(function(clickData) {
+    if (clickData.menuItemId == "Export") {
+      chrome.tabs.create({
+        url : '/html/export.html'
       });
     }
-  }
-});
+    if (clickData.menuItemId == "root" && clickData.selectionText) {
+      if (isInt(clickData.selectionText)) {
+        chrome.storage.sync.get([ 'total', 'limit' ], function(budget) {
+          var newTotal = 0;
+          if (budget.total) {
+            newTotal += parseInt(budget.total);
+          }
+          newTotal += parseInt(clickData.selectionText);
+          chrome.storage.sync.set({
+            'total' : newTotal
+          }, function() {
+            if (newTotal >= budget.limit) {
+              var notifOptions = {
+                type : "basic",
+                iconUrl : "icon48.png",
+                title : "Limit reached!",
+                message : "Uh oh, look's like you've reached your alloted limit."
+              };
+              chrome.notifications.create('limitNotif', notifOptions);
 
-chrome.storage.onChanged.addListener(function(changes, storageName) {
-  chrome.browserAction.setBadgeText({
-    "text" : changes.total !== undefined && changes.total.newValue !== undefined ? changes.total.newValue.toString() : ''
+            }
+          });
+        });
+      }
+    }
+  });
+
+  chrome.storage.onChanged.addListener(function(changes, storageName) {
+    chrome.browserAction.setBadgeText({
+      "text" : changes.total !== undefined && changes.total.newValue !== undefined ? changes.total.newValue.toString() : ''
+    });
   });
 });
