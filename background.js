91e663f2fcffe0a2dc396369a6efa155930fc726
Fernando Rodrigues da Roza <froza@padtec.com.br>
2018-12-04 23:04:57 -0200

    - Added menu
    Signed-off-by: Fernando Rodrigues da Roza <froza@padtec.com.br>

--- background.js
+++ background.js
@@ -1,4 +1,21 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "http://stackoverflow.com/";
-  chrome.tabs.create({ url: newURL });
+chrome.contextMenus.create({
+  "id": "Wikit",
+  "title": "Wikit",
+  "contexts": ["selection"]
 });
+
+chrome.contextMenus.create({
+  "id": "Export",
+  "title": "Export"
+});
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "Export"){
+      var newURL = "http://stackoverflow.com/";
+      chrome.tabs.create({ url: newURL });
+    } else if (clickData.menuItemId == "Wikit" && clickData.selectionText){
+        console.log('selected');
+    }
+});
+
+
