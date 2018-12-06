06804cb21bf9cb3df01a4c048f70de0d495ff3d3
Simeao Lamine <slamine@padtec.com.br>
2018-12-05 23:53:41 -0200

    fix event commit

--- eventListenner.js
+++ eventListenner.js
@@ -7,25 +7,25 @@ chrome.contextMenus.onClicked.addListener(function (clickData) {
                 });
             });
         }
+    }
 
-        if (clickData.parentMenuItemId == "itemPredicate" && clickData.selectionText) {
-            if (clickData.selectionText) {
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    chrome.tabs.sendMessage(tabs[0].id, { type: "itemPredicate", data: clickData }, function (response) {
-                        console.log(response);
-                    });
+    if (clickData.parentMenuItemId == "itemPredicate" && clickData.selectionText) {
+        if (clickData.selectionText) {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { type: "itemPredicate", data: clickData }, function (response) {
+                    console.log(response);
                 });
-            }
+            });
         }
+    }
 
-        if (clickData.parentMenuItemId == "itemObject" && clickData.selectionText) {
-            if (clickData.selectionText) {
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    chrome.tabs.sendMessage(tabs[0].id, { type: "itemObject", data: clickData }, function (response) {
-                        console.log(response);
-                    });
+    if (clickData.parentMenuItemId == "itemObject" && clickData.selectionText) {
+        if (clickData.selectionText) {
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { type: "itemObject", data: clickData }, function (response) {
+                    console.log(response);
                 });
-            }
+            });
         }
     }
 });
